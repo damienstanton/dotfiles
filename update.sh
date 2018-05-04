d4c9b25688ea2b21bf2fc5b330d929232f09920c
Damien Stanton <damien.stanton@gmail.com>
2018-05-04 11:32:06 -0400

    Dotfiles synced on 2018-05-04

--- update.sh
+++ update.sh
@@ -2,8 +2,10 @@
 set -e
 ZSH_FILE="$HOME/.zshrc"
 VIM_FILE="$HOME/.config/nvim/init.vim"
+TMUX_FILE="$HOME/.tmux.conf"
 cp $ZSH_FILE zsh/
 cp $VIM_FILE vim/
+cp $TMUX_FILE tmux/
 echo "Files are updated."
 echo "Uploading to GitHub..."
 git add -A && git commit -m "Dotfiles synced on $(date +%Y-%m-%d)" && git push
