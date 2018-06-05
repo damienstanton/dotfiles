c4478d88250ace2cc4d93da320b031d915247eba
Damien Stanton <damien.stanton@gmail.com>
2018-06-05 14:26:47 -0400

    Dotfiles synced on 2018-06-05

--- update.sh
+++ update.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -e
 ZSH_FILE="$HOME/.zshrc"
+ZSH_THEME_FILE="$HOME/.oh-my-zsh/custom/ds.zsh-theme"
 VIM_FILE="$HOME/.config/nvim/init.vim"
 TMUX_FILE="$HOME/.tmux.conf"
 cp $ZSH_FILE zsh/
+echo "Added ZSH config."
+cp $ZSH_THEME_FILE zsh/
+echo "Added custom ZSH theme."
 cp $VIM_FILE vim/
+echo "Added neovim config."
 cp $TMUX_FILE tmux/
-echo "Files are updated."
+echo "Added tmux config."
+echo "All files are updated."
 echo "Uploading to GitHub..."
 git add -A && git commit -m "Dotfiles synced on $(date +%Y-%m-%d)" && git push
 echo "👍  Done."
